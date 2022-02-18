4f93c7d68c617a29d529b0e9accf9bd3ac974bcb
Fernando Batista <fernandohsbatista@gmail.com>
2022-02-18 10:25:13 -0300

    refactor: set LogMongoRepository when creating SignUpController with LogControllerDecoration

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,15 +3,17 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DBAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BCryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../presentation/protocols'
 import { LogControllerDecorator } from '../decorators/log'
 
 export const makeSignUpController = (): Controller => {
   const addAccountRepository = new AccountMongoRepository()
+  const logMongoRepository = new LogMongoRepository()
   const salt = 12
   const bcryptAdapter = new BCryptAdapter(salt)
   const dbAddAccount = new DBAddAccount(bcryptAdapter, addAccountRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
