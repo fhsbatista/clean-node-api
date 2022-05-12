d120f82a2d82a16ad2dd1c5585f209d04331b5cf
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-12 19:46:00 -0300

    feat: move validation to a proper layer

--- src/main/factories/controllers/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/signup/signup-validation-factory.spec.ts
@@ -1,8 +1,8 @@
-import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../presentation/helpers/validators'
-import { EmailValidator } from '../../../../presentation/protocols/email-validator'
+import { CompareFieldsValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import { EmailValidator } from '../../../../validation/protocols/email-validator'
 import { makeSignUpValidation } from './signup-validation-factory'
 
-jest.mock('../../../../presentation/helpers/validators/validation-composite')
+jest.mock('../../../../validation/validators/validation-composite')
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
