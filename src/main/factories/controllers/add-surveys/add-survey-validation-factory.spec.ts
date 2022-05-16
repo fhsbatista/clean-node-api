0b93477e64e44e39d24275a2dbb20ba76f7ecf96
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-16 19:18:43 -0300

    feat: add AddSurvey composition on main layer

---
+++ src/main/factories/controllers/add-surveys/add-survey-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+describe('AddSurveyValidation Factory ', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+    expect(ValidationComposite).toHaveBeenCalledWith([
+      new RequiredFieldValidation('question'),
+      new RequiredFieldValidation('answers')
+    ])
+  })
+})
---
+++ src/main/routes/surveys-routes.test.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let surveysCollection
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    surveysCollection = await MongoHelper.getCollection('surveys')
+    await surveysCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('POST /surveys] ', () => {
+    test('Should return 200 on surveys', async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'answer 1',
+            image: 'http://image-path.com'
+          },
+          {
+            answer: 'answer 2'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
