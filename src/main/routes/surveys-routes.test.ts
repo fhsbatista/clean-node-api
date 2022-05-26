fff1e35ce0134c83219cccb54e059932555ded97
Fernando Batista <fernandohsbatista@gmail.com>
2022-05-26 10:50:41 -0300

    test: ensure add survey route returns 403 if no access token is provided

--- src/main/routes/surveys-routes.test.ts
+++ src/main/routes/surveys-routes.test.ts
@@ -19,7 +19,7 @@ describe('Survey Routes', () => {
   })
 
   describe('POST /surveys] ', () => {
-    test('Should return 200 on surveys', async () => {
+    test('Should return 403 on add_survey without access token', async () => {
       await request(app)
         .post('/api/surveys')
         .send({
@@ -32,7 +32,7 @@ describe('Survey Routes', () => {
             answer: 'answer 2'
           }]
         })
-        .expect(204)
+        .expect(403)
     })
   })
 })
